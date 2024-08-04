058447e6292aafeeba88207af4f8d69fb55c897a
Faizanahmedsy <saiyed.faizanahmed1@gmail.com>
2024-08-04 11:32:30 +0530

    wmfdmsidmjsmdjdwd

--- app/page.jsx
+++ app/page.jsx
@@ -1,3 +1,4 @@
+import Card from "@/components/card";
 import Image from "next/image";
 
 export default function Home() {
@@ -7,12 +8,14 @@ export default function Home() {
       <section className="min-h-screen grid place-content-center">
         <h1 className="text-8xl font-bold   border-2 border-red-400 ">Kavya</h1>
       </section>
-      <section className="grid grid-cols-1   md:grid-cols-2 lg:grid-cols-5 min-h-screen place-content-center px-32">
-        <div className="border-2 border-violet-800 rounded-2xl p-4 text-center font-medium">Javscript</div>
-        <div>React</div>
-        <div>Tailwind</div>
-        <div>Next</div>
-        <div>Node</div>
+      <section className="flex justify-evenly items-center  ">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5 gap-6 py-10 px-32">
+          <Card name={"Javascript"} />
+          <Card name={"React"} />
+          <Card name={"Next"} />
+          <Card name={"Tailwind"} />
+          <Card name={"Node"} />
+        </div>
       </section>
     </div>
   );
--- components/card.jsx
+++ components/card.jsx
@@ -1,3 +1,9 @@
-export default function Card() {
-    return <h1>Card</h1>
+export default function Card({ name }) {
+  return (
+    <>
+      <div className="border-2 border-violet-800 rounded-2xl p-4 text-center font-medium">
+        {name}
+      </div>
+    </>
+  );
 }
